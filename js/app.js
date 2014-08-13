0cc82938bd36d10e39a3eb9266abdda67666376b
solo206 <jsolk206@gmail.com>
2014-08-12 19:40:46 -0700

    refactored fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
-	$('.querybox').keypress(function(event){
-		var keyvalue=13;
-		if (event.which==keyvalue){
-			event.preventDefault();
-			alert("entered");
-			var stringmax=$('.querybox').val();
-			// alert(stringmax);
-			var max=parseInt(stringmax);
+	// $('.querybox').click(function(event){
+		// var keyvalue=13;
+		// if (event.which==keyvalue){
+			// event.preventDefault();
+			// var stringmax=$('.querybox').val();
+			var stringmax=prompt("Please enter a number from 1-100:");
+			var rawNum=parseInt(stringmax);
+			var max=checkNum(rawNum);
+
+			confirm("So "+max+" is the number you wanted me to enter?")
+			alert(""+max+".  Great, your fizzbuzz is:");
 			// alert(max);
-			alert(max);
 			loop(max);
 
-		}
-	});
+		// }
+	// });
 		
 		// var result="";
 		// result+=""+max;
@@ -21,6 +23,15 @@ $(document).ready(function(){
 	// $(".response").text(result);
 
 });
+function checkNum(rawNum){
+	//checks if number, if it is a number, returns number back number to max variable
+	//if not number loops prompt asking for new number, then return that number as max
+	while(isNaN(rawNum)){
+		rawNum=prompt("This is not a number between 1-100 please reenter number:");
+	}
+	return rawNum;
+
+}
 function loop (max){
 		var result="";
 		for(var count=1;count<=max;count++){
